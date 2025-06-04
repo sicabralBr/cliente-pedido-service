af00ba1462251d227611de774660d6a43efe1e9f
gabri <gabriel.mello@quality.com.br>
2025-06-04 20:59:42 -0300

    - modularizacao correta

--- cliente-pedido-service/logistica/src/main/java/com/desafiosantander/application/service/LogisticaService.java
+++ cliente-pedido-service/logistica/src/main/java/com/desafiosantander/application/service/LogisticaService.java
@@ -4,11 +4,13 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import com.desafiosantander.application.dto.PedidoCriadoEvent;
 import com.desafiosantander.infrastructure.PedidoClient;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @ApplicationScoped
 public class LogisticaService {
 
     @Inject
+    @RestClient
     PedidoClient pedidoClient;
 
     public void processar(PedidoCriadoEvent evento) {
--- cliente-pedido-service/logistica/src/main/java/com/desafiosantander/infrastructure/PedidoClient.java
+++ cliente-pedido-service/logistica/src/main/java/com/desafiosantander/infrastructure/PedidoClient.java
@@ -3,14 +3,13 @@ package com.desafiosantander.infrastructure;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-@Path("/api/pedidos")
+@RegisterRestClient
+@Path("/pedidos")
 public interface PedidoClient {
 
     @PUT
     @Path("/{id}/status")
-    @Consumes(MediaType.TEXT_PLAIN)
-    void atualizarStatus(@PathParam("id") Long pedidoId, String novoStatus);
+    void atualizarStatus(@PathParam("id") Long id, String status);
 }
